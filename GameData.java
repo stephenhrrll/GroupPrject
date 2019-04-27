3ec1a63aaa05f9ab41aeeacc48166a6051677a22
Stephen <sh2600@uncw.edu>
2019-04-26 21:35:43 -0400

    Added an ArrayList attrubute, constructor and a getter

--- GameData.java
+++ GameData.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class GameData {
+
+    ArrayList<Player> gameData;
+
+    public GameData(){
+        gameData = new ArrayList<>();
+    }
+
+    public ArrayList<Player> getGameData() {
+        return gameData;
+    }
 }
