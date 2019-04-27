3216c5a0921aa8bebbf226b9afc895f47e8c8d4a
Stephen <sh2600@uncw.edu>
2019-04-27 19:54:53 -0400

    added find and add methods

--- GameData.java
+++ GameData.java
@@ -2,13 +2,26 @@ import java.util.ArrayList;
 
 public class GameData {
 
-    ArrayList<Player> gameData;
+    ArrayList<Player> players;
 
     public GameData(){
-        gameData = new ArrayList<>();
+        players = new ArrayList<>();
     }
 
     public ArrayList<Player> getGameData() {
-        return gameData;
+        return players;
+    }
+
+    public Player find(String name){
+        for(Player p:players){
+            if(p.getName().matches(name)){
+                return p;
+            }
+        }
+        return null;
+    }
+
+    public void add(Player p){
+        players.add(p);
     }
 }
