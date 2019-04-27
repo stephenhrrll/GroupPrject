5e421511be6de26c77341420e01a06c17348fddf
Stephen <sh2600@uncw.edu>
2019-04-26 21:46:27 -0400

    Added two attributes, a constructor, setters and getters

--- Player.java
+++ Player.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class Player {
+
+    String name;
+    ArrayList<GamePlayed> gamesPlayed;
+
+    public Player(String name){
+        this.name = name;
+        this.gamesPlayed = new ArrayList<GamePlayed>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<GamePlayed> getGamesPlayed() {
+        return gamesPlayed;
+    }
+
+    public void setGamesPlayed(ArrayList<GamePlayed> gamesPlayed) {
+        this.gamesPlayed = gamesPlayed;
+    }
 }
