f68293a2da493ae02a6bfd55e395d9177912b7d7
Stephen <sh2600@uncw.edu>
2019-05-01 08:38:00 -0400

    Evans changes

--- Messages.java
+++ Messages.java
@@ -14,15 +14,21 @@ public class Messages {
     }
 
     public String enterLevelInstructions(){
-        return "Explain what the levels are and how to proceed";
+        return "Enter what level you want your game to be. 1 being the lowest.";
     }
 
     public String levelOneInstructions(){
-        return "Level 1 instructions go here";
+        return "For level one, Enter a positive whole number which you want to be the maximum value of the range that a random number will be generated.\n" +
+                "The minimum value will always be 1. Once the number is generated you will have to guess the number, after each guess a hint will be given such as 'Too high' or 'Too low'\n"
+                + "Once you guess correctly, a 'Win' message will be displayed and the amount of guesses you made and the time you took will be recorded";
     }
 
     public String higherLevelInstructions(){
-        return "higher level instructions go here";
+        return "Enter a whole number which will represent the level. The value of that whole number will represent how many numbers between 0 and 9 you will have to guess\n"
+                + "  For example. If you choose level 3, the numbers 826 could be an example of the numbers generated. \n"
+                + " a 2 digit response will be given, the first number representing how many numbers were guessed correctly\n"
+                + " and the second representing how many were in the right position. For example. Guessing 806 will return 22 \n" +
+                " and guessing 569 would return 1,0. Finally, guessing 826 would return a 'Win'";
     }
 
     public String winMessage(){
@@ -33,4 +39,3 @@ public class Messages {
 
 
 }
-
